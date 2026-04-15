e3377ac1d49b021dbd9837554856f0945505e6b7
Bodebate <601463@student.belgiumcampus.ac.za>
2026-04-15 18:51:29 +0200

    started correcting the file structure

--- SRC/preprocess_data.py
+++ SRC/preprocess_data.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report
 
 #loading dataset
 df = pd.read_csv("Diabetes_and_LifeStyle_Dataset.csv")
@@ -43,30 +39,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#Random Forest
-rfModel = RandomForestClassifier(random_state=42)
-rfModel.fit(X_train, y_train)
-rfPred = rfModel.predict(X_test)
-rfAccuracy = accuracy_score(y_test, rfPred)
-
-#Decision Tree
-dtModel = DecisionTreeClassifier(random_state=42)
-dtModel.fit(X_train, y_train)
-dtPred = dtModel.predict(X_test)
-dtAccuracy = accuracy_score(y_test, dtPred)
-
-print("Random Forest Accuracy:", rfAccuracy)
-print("Decision Tree Accuracy:", dtAccuracy)
-print(classification_report(y_test, rfPred))
-
-#saving the best model and metadata
-modelBundle = {
-    "model": rfModel,
-    "featureColumns": list(X.columns),
-    "categoricalColumns": categoricalColumns,
-    "categoryMaps": categoryMaps,
-    "targetMap": targetMap
-}
-
-joblib.dump(modelBundle, "diabetesModel.pkl")
-print("Model saved successfully!")
+#saving data split to csv
+pd.concat([X_test,y_test],axis=1).to_csv(path_or_buf="DATA/Test.csv",index = False)
+pd.concat([X_train,y_train],axis=1).to_csv(path_or_buf="DATA/Train.csv",index = False)
